920346654024d15d354d372a47922b02ae2577fc
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-16 19:06:01 +0900

    Class Components(クラスコンポーネント)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Learn from './components/Hello'
+import Learning from './components/Learning';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Learn />
+        <Learning />
       </header>
     </div>
   );
