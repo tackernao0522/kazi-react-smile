5e50a7c33ca847fec3b02958d3a00f65ecf03845
takaki <cheap_trick_magic@yahoo.co.jp>
2021-11-17 16:25:00 +0900

    State Manipulating

--- README02.md
+++ README02.md
@@ -266,3 +266,50 @@ class Learning extends Component {
 
 export default Learning
 ```
+
+## State Manipulating
+
++ `src/components/Learning.jsx`を編集<br>
+
+```
+import React, { Component } from 'react'
+
+class Learning extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            fruits: [
+                { name: 'Mango', weight: '120gm' },
+                { name: 'Apple', weight: '20gm' },
+                { name: 'Orange', weight: '60gm' },
+            ],
+        }
+    }
+
+    clickHandler = () => {
+    // console.log('Hello Easy Learning')
+    // alert('Welcome Udemy')
+    // this.state.fruits[0] = 'samsung';
+    this.setState({
+        fruits: [
+                { name: 'Samsung', weight: '120gm' },
+                { name: 'Apple', weight: '20gm' },
+                { name: 'Nokia', weight: '60gm' },
+            ],
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <button onClick={this.clickHandler}>Click Me</button>
+            <h1>Fruit name is {this.state.fruits[0].name}</h1>
+            <h1>Fruit name is {this.state.fruits[1].name}</h1>
+            <h1>Fruit name is {this.state.fruits[2].name}</h1>
+        </div>
+        )
+    }
+}
+
+export default Learning
+```
--- src/components/Learning.jsx
+++ src/components/Learning.jsx
@@ -4,17 +4,25 @@ class Learning extends Component {
     constructor(props) {
         super(props)
         this.state = {
-        fruits: [
-            { name: 'Mango', weight: '120gm' },
-            { name: 'Apple', weight: '20gm' },
-            { name: 'Orange', weight: '60gm' },
-        ],
+            fruits: [
+                { name: 'Mango', weight: '120gm' },
+                { name: 'Apple', weight: '20gm' },
+                { name: 'Orange', weight: '60gm' },
+            ],
         }
     }
 
     clickHandler = () => {
-        console.log('Hello Easy Learning')
-        alert('Welcome Udemy')
+    // console.log('Hello Easy Learning')
+    // alert('Welcome Udemy')
+    // this.state.fruits[0] = 'samsung';
+    this.setState({
+        fruits: [
+                { name: 'Samsung', weight: '120gm' },
+                { name: 'Apple', weight: '20gm' },
+                { name: 'Nokia', weight: '60gm' },
+            ],
+        })
     }
 
     render() {
@@ -22,6 +30,8 @@ class Learning extends Component {
         <div>
             <button onClick={this.clickHandler}>Click Me</button>
             <h1>Fruit name is {this.state.fruits[0].name}</h1>
+            <h1>Fruit name is {this.state.fruits[1].name}</h1>
+            <h1>Fruit name is {this.state.fruits[2].name}</h1>
         </div>
         )
     }
