5d521e65938e7ba795fee7cddab5d849314d958e
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-17 18:58:04 +0900

    Stylesheet & Inline Style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import Check from './components/Check';
+// import Check from './components/Check';
 // import logo from './logo.svg';
 // import Learn from './components/Hello'
-// import Learning from './components/Learning';
+import Learning from './components/Learning';
 // import Hook from './components/Hook';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Check />
+        <Learning />
       </header>
     </div>
 
