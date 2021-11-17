773bdb7f1e51b5943d9e0f5643663683c099675a
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-17 20:44:16 +0900

    Multiple Input

--- README02.md
+++ README02.md
@@ -777,3 +777,55 @@ class App extends React.Component {
 export default App;
 ```
 
+## Multiple Input
+
++ `App.js`を編集<br>
+
+```
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    username: "",
+    phonenumber: "",
+    email: "",
+    city: ""
+  }
+
+  ChangeData = (e) => {
+    let val = e.target.name; // Username, phonenumber, email (e.target.nameのnameはname属性)
+    let myValue = e.target.value // Gettin value (name属性の値)
+    this.setState({ [val]: myValue }); // value set on the state (name="myValue")
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{ this.state.username}</h1>
+        <h1>{ this.state.phonenumber}</h1>
+        <h1>{ this.state.email}</h1>
+        <h1>{ this.state.city}</h1>
+        <div>
+          <form>
+            <div class="form-group" style={{ marginLeft: 200 }}>
+              <label>User Name:</label>
+              <br />
+              <input type="text" name="username" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="phonenumber" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="email" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="city" onChange={this.ChangeData}></input>
+              <br />
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
+```
--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    username: ""
+    username: "",
+    phonenumber: "",
+    email: "",
+    city: ""
   }
 
   ChangeData = (e) => {
-    let val = e.target.name; // Username
-    let myValue = e.target.value // Gettin value
-    this.setState({ [val]: myValue }); // value set on the state
-  }
-
-  formSubmit = (event) => {
-    event.preventDefault(); // リロードされないようにする
-    console.log(this.state.username);
+    let val = e.target.name; // Username, phonenumber, email (e.target.nameのnameはname属性)
+    let myValue = e.target.value // Gettin value (name属性の値)
+    this.setState({ [val]: myValue }); // value set on the state (name="myValue")
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello {this.state.username}</h1>
+        <h1>{ this.state.username}</h1>
+        <h1>{ this.state.phonenumber}</h1>
+        <h1>{ this.state.email}</h1>
+        <h1>{ this.state.city}</h1>
         <div>
-          <form onSubmit={this.formSubmit}>
+          <form>
             <div class="form-group" style={{ marginLeft: 200 }}>
               <label>User Name:</label>
               <br />
               <input type="text" name="username" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="phonenumber" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="email" onChange={this.ChangeData}></input>
+              <br />
+              <input type="text" name="city" onChange={this.ChangeData}></input>
               <br />
               <button type="submit">Submit</button>
             </div>
