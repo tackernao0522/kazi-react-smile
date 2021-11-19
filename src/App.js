9e2006fe7e5d874223886f439076aec5659eb48b
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-19 19:30:36 +0900

    Form Validation

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ class App extends React.Component {
   state = {
     email: "",
     password: "",
+    emailError: '',
+    passwordError: ''
+  }
+
+  validate() {
+    if () {
+
+    } else if () {
+
+    } else {
+      return true;
+    }
   }
 
   formSumbit = (e) => {
     e.preventDefault();
-    console.log(this.state.email, this.state.password);
+    if (this.validate()) {
+      alert("Form Submit Done");
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{ this.state.email}</h1>
-        <h1>{ this.state.password}</h1>
+        <h1>Login Panel</h1>
         <div>
           <form onSubmit={this.formSumbit}>
             <div class="form-group" style={{ marginLeft: 200 }}>
-              <label>User Name:</label>
+              <label>Email:</label>
               <br />
-              <input type="email" name="email" onChange={(event) => {this.setState({email: event.target.value})}}></input>
+              <input type="email" name="email" onChange={(event) => { this.setState({ email: event.target.value }) }}></input>
               <br />
-              <input type="passoword" name="password" onChange={(event) => {this.setState({password: event.target.value})}}></input>
+              <strong style={{ color: 'red' }}>Email and Password Invalid</strong>
+              <br />
+              <label>Password:</label>
+              <br />
+              <input type="passoword" name="password" onChange={(event) => { this.setState({ password: event.target.value }) }}></input>
+              <br />
+              <strong style={{ color: 'red' }}>Email and Password Invalid</strong>
               <br />
               <button type="submit">Submit</button>
             </div>
