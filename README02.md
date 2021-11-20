ad618eaaacfa4b5c7692b66f99c2b4ce3208e01c
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-20 17:08:01 +0900

    Lists

--- README02.md
+++ README02.md
@@ -1091,3 +1091,29 @@ class App extends React.Component {
 
 export default App;
 ```
+## Lists
+
++ `App.js`を編集<br>
+
+```
+import React from "react";
+
+class App extends React.Component {
+
+  render() {
+    const names = ['AA', 'BB', 'CC', 'DD', 'EE']
+    const items = names.map((item, idx) => <li key={idx}>{idx}-{item}</li>)
+
+    return (
+      <div className="App">
+        <h1>Student List</h1>
+        <div>
+          {items}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
+```
--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    age: ""
-  }
-
-  formSumbit = (e) => {
-    e.preventDefault();
-  }
 
   render() {
+    const names = ['AA', 'BB', 'CC', 'DD', 'EE']
+    const items = names.map((item, idx) => <li key={idx}>{idx}-{item}</li>)
+
     return (
       <div className="App">
-        <h1>Select Option</h1>
+        <h1>Student List</h1>
         <div>
-          <form onSubmit={this.formSumbit}>
-            <div class="form-group" style={{ marginLeft: 200 }}>
-              <label>Your Age:</label>
-              <br />
-              <select onChange={(event) => {this.setState({age: event.target.value})}}>
-                <option value="" selected disabled>Select Your Age</option>
-                <option value="15 - 20">15 - 20</option>
-                <option value="21 - 30">21 - 30</option>
-                <option value="31 - 40">31 - 40</option>
-              </select>
-              <br />
-              <h6>My Age : {this.state.age}</h6>
-              <button type="submit">Submit</button>
-            </div>
-          </form>
+          {items}
         </div>
       </div>
     )
