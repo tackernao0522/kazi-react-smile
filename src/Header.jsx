adc19da5b6f0f8ac0e00ba523bc744a3ccb692bc
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-21 18:53:00 +0900

    ActiveLink

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom'
 import App from './App'
 import About from './components/About'
 import Blog from './components/Blog'
@@ -14,14 +14,14 @@ export default class Header extends Component {
       <Router>
         <div>
           <Navbar bg="light" expand="lg">
-            <Navbar.Brand><Link to="/">Easy Learning</Link></Navbar.Brand>
+            <Navbar.Brand><NavLink exact activeStyle={{color: 'orange'}} to="/">Easy Learning</NavLink></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link><Link to="/about">About Us</Link></Nav.Link>
-                <Nav.Link><Link to="/contact">Contact Us</Link></Nav.Link>
-                <Nav.Link><Link to="/blog">Blog</Link></Nav.Link>
-                <Nav.Link><Link to="/profile">Profile</Link></Nav.Link>
+                <Nav.Link><NavLink activeStyle={{color: 'orange'}} to="/about">About Us</NavLink></Nav.Link>
+                <Nav.Link><NavLink activeStyle={{color: 'orange'}} to="/contact">Contact Us</NavLink></Nav.Link>
+                <Nav.Link><NavLink activeStyle={{color: 'orange'}} to="/blog">Blog</NavLink></Nav.Link>
+                <Nav.Link><NavLink activeStyle={{color: 'orange'}} to="/profile">Profile</NavLink></Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
